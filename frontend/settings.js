b06769129507ba31df288bca06c5e8664c28db59
phrosyne <mcoderej@gmail.com>
2025-09-07 21:29:08 -0700

    settings boilerplate updated

---
+++ frontend/settings.js
@@ -0,0 +1,6 @@
+const lightButton = document.querySelector('#light');
+const darkButton = document.querySelector('#dark');
+
+const weather = document.querySelector('#weather');
+
+const news = document.querySelector('#news-container');
